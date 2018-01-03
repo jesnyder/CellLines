38c9077fbbf6d96db0e91bcb2a521ea7d450cc32
Jesyndre <jessicaesnyder@gmail.com>
2018-01-03 11:17:50 -0800

    Moved files into folders for organization

--- Counts/BuildTables.m
+++ Counts/BuildTables.m
@@ -17,7 +17,10 @@ while(i <= max(B(:,2)))
 end
 
 A = A(min(B(:,2)):end,:); A = [A(:,1), transpose(sortrows(transpose(A(:,2:end)),-2))]
-csvwrite('BC/vBC_PatCount.csv',A)
+
+header = {'Year','Unknown','White','Black','Asian','Hispanic'};
+R = [header; num2cell(A)]
+csvwrite('BC/vBC_PatCount.csv',R)
 
 PatCount = csvread('Originals/v01_PuCo.csv', 4, 0);
 B = PatCount; A = []; i = min(B(:,2)); 
@@ -36,37 +39,8 @@ end
 A = A(min(B(:,2)):end,:); A = [A(:,1), transpose(sortrows(transpose(A(:,2:end)),-2))]
 csvwrite('BC/vBC_PubCount.csv',A)
 
-%% Prostate Cancer
-PatCount = csvread('Originals/v01_PaCo.csv', 4, 0);
-B = PatCount; A = []; i = min(B(:,2)); 
-B=B(B(:,3) == 2,:);  B=[B(:,2) B(:,4:5)];
 
-while(i <= max(B(:,2)))
-    C=B(B(:,2) == i,:); A(i, 1) = i; j=1; 
-    
-    while(j<=max(B(:,1)))
-        D=C(C(:,1) == j,:); A(i, j+1) = sum(D(:,3)); j=j+1;
-    end
-    
-    i=i+1;  
-end
 
-A = A(min(B(:,2)):end,:); A = [A(:,1), transpose(sortrows(transpose(A(:,2:end)),-2))]
-csvwrite('PC/vPC_PatCount.csv',A)
 
-PatCount = csvread('Originals/v01_PuCo.csv', 4, 0);
-B = PatCount; A = []; i = min(B(:,2)); 
-B=B(B(:,3) == 2,:);  B=[B(:,2) B(:,4:5)];
 
-while(i <= max(B(:,2)))
-    C=B(B(:,2) == i,:); A(i, 1) = i; j=1; 
-    
-    while(j<=max(B(:,1)))
-        D=C(C(:,1) == j,:); A(i, j+1) = sum(D(:,3)); j=j+1;
-    end
-    
-    i=i+1;  
-end
 
-A = A(min(B(:,2)):end,:); A = [A(:,1), transpose(sortrows(transpose(A(:,2:end)),-2))]
-csvwrite('PC/vPC_PubCount.csv',A)
