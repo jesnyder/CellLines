08035a369d7bec5b26e5d0151afb1bf78bb53b97
Jesyndre <jessicaesnyder@gmail.com>
2018-01-03 17:48:37 -0800

    Working on the csv files

--- Counts/CSVFunction.m
+++ Counts/CSVFunction.m
@@ -1,13 +1,16 @@
+%Reformat csv to present the 
+
 function  CVSFunction(a,b,x)
 
+% Read in the csv
 B = csvread(a, 1, 0);
 
-A = []; i = min(B(:,2)); 
+% Truncate for cancer type, remove gender and cancer index columns
+B = B(B(:,3) == x,:);  B=[B(:,2) B(:,4:5)];
 
-B=B(B(:,3) == x,:);  
-
-B=[B(:,2) B(:,4:5)];
-
+% Populate a matrix with counts per year for each ethniciy
+% Column heading ethnicities, First rows is each year
+i = min(B(:,2)); A = [];
 while(i <= max(B(:,2)))
     
     C=B(B(:,2) == i,:); A(i, 1) = i; j=1; 
@@ -22,22 +25,16 @@ while(i <= max(B(:,2)))
     
 end
 
+A = A(min(B(:,2)):end,:); 
 
-A = A(min(B(:,2)):end,:) ;
+header = {'Year','Asian','Black','Hispanic','White','Unknown'};
 
-header = {'Year','Unknown','White','Black','Asian','Hispanic'};
+% Create a table, then sort
+R = array2table([header; num2cell(A)]) ;
+R = [R(:,1),R(:,5),R(:,6),R(:,3),R(:,2),R(:,4)]; 
 
-T = table(header);
-T1 = table(transpose(A));
-T1.Properties.RowNames = header;
-
-T = A; 
-
-A = [A(:,1), transpose(sortrows(transpose(A(:,2:end)),-2))];
-
-
-writetable(array2table([header; num2cell(A)]),b,'Delimiter',',');
-
+% Write the table to a csv
+writetable(R,b,'Delimiter',',');
 
 end
 
